d46fff1f016b641fcc8bbf1818bb41adbccec7f2
karamomo.kana <karamomokk@icloud.com>
2019-08-13 20:58:47 +0900

    新規画面

---
+++ derbo/DetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailViewController.swift
+//  derbo
+//
+//  Created by カナ on 2019/08/02.
+//  Copyright © 2019 Kanako Ikegami. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    var moneyDate:[String] = []
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return moneyDate.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}

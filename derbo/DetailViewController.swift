70822b0c4e5a62f1d68124b9902c4bdc01379e4b
karamomo.kana <karamomokk@icloud.com>
2019-08-28 09:50:04 +0900

    Assetsがないというエラー？解消できるか、コミットしてみる

--- derbo/DetailViewController.swift
+++ derbo/DetailViewController.swift
@@ -77,4 +77,14 @@ class DetailViewController : UIViewController, UITableViewDelegate, UITableViewD
         detailDate.remove(at: indexPath.row)
         detailTableView.deleteRows(at: [indexPath], with: .fade)
     }
+    
+    //タップして画面推移
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // セルの選択を解除
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // 別の画面に遷移
+        performSegue(withIdentifier: "modifyPage", sender: nil)
+    }
 }
